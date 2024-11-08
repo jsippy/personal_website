afb2f7817ff4b2781820bf44e33f04b8816a486c
jsippy <jacob.d.sippy@gmail.com>
2024-11-07 21:26:38 -0800

    Add publication to projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,6 +5,10 @@ export default function Projects() {
       <ul>
         <li><a href="https://github.com/jsippy/">This Website</a><br/>Built with React and hosted on a private server. Deployed with Github Pages.</li>
       </ul>
+      <h1>Publications</h1>
+      <ul>
+        <li><a href="https://api.semanticscholar.org/CorpusID:231843293">Data Staining: A Method for Comparing Faithfulness of Explainers</a></li>
+      </ul>
     </div>
   );
 }
